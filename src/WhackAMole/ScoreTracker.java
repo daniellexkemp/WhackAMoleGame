01620ecb5716672baa63a0c44702560151dd67da
Danielle Kemp <146593026+daniellexkemp@users.noreply.github.com>
2025-12-14 11:33:12 -0600

    Refactor ScoreTracker class documentation and comments

--- src/WhackAMole/ScoreTracker.java
+++ src/WhackAMole/ScoreTracker.java
@@ -1,57 +1,53 @@
-// Project: Whack-A-Mole Game
-// Author: Susan Rogers
-// Date: December 2, 2025
-// Description: The ScoreTracker class is responsible for keeping track of the player's score
-// during the Whack-A-Mole game.
-************************************************************************************************
-public class ScoreTracker {
- // Current score
-    private int score;
+/***********************************************************
+ * Team: G1
+ * Project: WhackAMoleGame
+ * Author: Susan Rogers
+ * Date: 12/13/2025
+ * Class: ScoreTracker
+ * Description:
+ ***********************************************************/
+package WhackAMole;
 
+public class ScoreTracker {
+    // Current score
+    private int score;
     /**
-     * Default constructor initializes score to zero.(stub)
+     * Default constructor initializes score to zero.
      */
     public ScoreTracker() {
         this.score = 0;
     }
-
     /**
-     * Called when the player successfully whacks a mole. Adds 2 points per hit(stub)
+     * Called when the player successfully whacks a mole. Adds 2 points per hit
      * (adjust if desired).
      */
     public void hit() {
         score += 2;
     }
-
     /**
      * Called when the player misses. Subtracts 1 point per miss, but never
-     * below zero.(stub)
+     * below zero.
      */
     public void miss() {
         if (score > 0) {
             score -= 1;
         }
     }
-
     /**
-     * @return the current score value.(stub)
+     * @return the current score value.
      */
     public int getScore() {
         return score;
     }
-
     /**
-     * Resets score to zero for a new game.(stub)
+     * Resets score to zero for a new game.
      */
     public void reset() {
         score = 0;
     }
-
     @Override
     public String toString() {
         return "Score: " + score;
     }
-}
-    
 }
 
